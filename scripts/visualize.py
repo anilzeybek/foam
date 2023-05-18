295fc6b9324eb5724461bbbbfb7eeb4831754777
Zak Kingston <zak@rice.edu>
2023-05-18 17:46:32 -0500

    reorg

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -18,8 +18,7 @@ def main(mesh: str | None = None, spheres: str | None = None, level: int = 1):
         if not mesh_filepath.exists:
             raise RuntimeError(f"Path {mesh} does not exist!")
 
-        loaded_mesh = as_mesh(load_mesh(mesh_filepath,
-                                        process=False))  # type: ignore
+        loaded_mesh = as_mesh(load_mesh(mesh_filepath, process = False)) # type: ignore
 
         scene.add_geometry(loaded_mesh)
 
@@ -29,19 +28,17 @@ def main(mesh: str | None = None, spheres: str | None = None, level: int = 1):
             raise RuntimeError(f"Path {spheres} does not exist!")
 
         with open(sphere_filepath, 'r') as json_file:
-            data = jsload(json_file, cls=SphereDecoder)
+            data = jsload(json_file, cls = SphereDecoder)
 
         if level > len(data):
-            raise RuntimeError(
-                f"Level {level} greater than available ({len(data)})!")
+            raise RuntimeError(f"Level {level} greater than available ({len(data)})!")
         sphere_data = data[level]['spheres']
 
         cm = mpl.colormaps['viridis']
         for sphere in sphere_data:
-            sphere_mesh = icosphere(radius=sphere.radius)
+            sphere_mesh = icosphere(radius = sphere.radius)
             sphere_mesh.visual.face_colors = [255 * c for c in cm(random.uniform(0, 1))][:3] + [100]
-            scene.add_geometry(sphere_mesh,
-                               transform=translation_matrix(sphere.origin))
+            scene.add_geometry(sphere_mesh, transform = translation_matrix(sphere.origin))
 
     if mesh or spheres:
         SceneViewer(scene)
