dd9603d31feba1612f45d4135453517a867a0a1b
Zak Kingston <zak@rice.edu>
2023-05-18 14:06:29 -0500

    working scripts

--- scripts/spheres.py
+++ scripts/spheres.py
@@ -1,81 +1,36 @@
-from contextlib import contextmanager
-from dataclasses import dataclass
-from pathlib import Path
-from tempfile import NamedTemporaryFile
-
+from json import dumps
 from os import remove
-from json import dumps, JSONEncoder
+from pathlib import Path
 from subprocess import run
+
 from fire import Fire
-from trimesh.scene.scene import Scene
-from trimesh.base import Trimesh
 from trimesh.exchange.load import load_mesh
 from trimesh.exchange.obj import export_obj
-from trimesh.util import concatenate
 from trimesh.voxel.creation import voxelize
 from trimesh.voxel.ops import matrix_to_marching_cubes
 
-
-@dataclass(slots=True)
-class Sphere:
-    origin: tuple[float, float, float]
-    radius: float
-
-    def __init__(self, x: float, y: float, z: float, r: float):
-        self.origin = (x, y, z)
-        self.radius = r
-
-
-class SphereEncoder(JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj, Sphere):
-            return {'origin': obj.origin, 'radius': obj.radius}
-
-        return JSONEncoder.default(self, obj)
-
-
-@contextmanager
-def tempmesh():
-    f = NamedTemporaryFile('w', suffix=f'.obj')
-    try:
-        yield f, Path(f.name)
-    finally:
-        f.close()
-
-
-def as_mesh(scene_or_mesh: Trimesh | Scene) -> Trimesh:
-    if isinstance(scene_or_mesh, Scene):
-        if len(scene_or_mesh.geometry) == 0:
-            raise RuntimeError("Opened an empty mesh file!")
-        else:
-            return concatenate(
-                tuple(
-                    Trimesh(vertices=g.vertices, faces=g.faces)
-                    for g in scene_or_mesh.geometry.values()))  # type: ignore
-    else:
-        return scene_or_mesh
-
-
-def make_watertight(mesh: Trimesh, resolution: float = 0.01) -> Trimesh:
-    voxels = voxelize(mesh, resolution)
-    voxels.fill()
-    return matrix_to_marching_cubes(voxels.matrix, resolution)
+from common import *
 
 
 def main(mesh: str,
          output: str | None = None,
-         depth: int = 2,
-         branch: int = 10,
-         tester_level: int = 1):
+         resolution: float = 0.01,
+         depth: int = 1,
+         branch: int = 8,
+         tester_level: int = 2):
     mesh_filepath = Path(mesh)
     if not mesh_filepath.exists:
         raise RuntimeError(f"Path {mesh} does not exist!")
 
     loaded_mesh = as_mesh(load_mesh(mesh_filepath,
                                     process=False))  # type: ignore
-    watertight_mesh = loaded_mesh if loaded_mesh.is_watertight else make_watertight(
-        loaded_mesh)
+
+    if not loaded_mesh.is_watertight:
+        voxels = voxelize(loaded_mesh, resolution)
+        voxels.fill()
+        watertight_mesh = matrix_to_marching_cubes(voxels.matrix, resolution)
+    else:
+        watertight_mesh = loaded_mesh
 
     _ = watertight_mesh.vertex_normals  # Need to compute vertex normals
     with tempmesh() as (input_mesh, input_path):
@@ -83,7 +38,7 @@ def main(mesh: str,
         input_mesh.flush()
 
         run([
-            './build/spheretree/makeTreeMedial',
+            '../build/spheretree/makeTreeMedial',
             '-branch',
             str(branch),
             '-depth',
@@ -103,10 +58,17 @@ def main(mesh: str,
             '-nopause',
             '-expand',
             '-merge',
-            str(input_path),
+            '-optimise',
+            'simplex',
+            '-maxOptLevel',
+            '1',
+            str(input_path)
         ])
 
         output_file = input_path.parent / (input_path.stem + '-medial.sph')
+
+        low_bounds, high_bounds = loaded_mesh.bounds
+        offset = (high_bounds + low_bounds) / 2
 
         with open(output_file, 'r') as output_spheres:
             lines = output_spheres.readlines()
@@ -123,9 +85,11 @@ def main(mesh: str,
                     zip(spheres_per_level, mean_error, strict=True)):
                 start = 1 + sum(spheres_per_level[:i])
                 spheres = [
-                    Sphere(*list(map(float, line.split()))[:-1])
+                    Sphere(*list(map(float, line.split()))[:-1], offset)  # type: ignore
                     for line in lines[start:start + level]
                 ]
+
+                spheres = list(filter(lambda s: s.radius > 0, spheres))
 
                 output_json.append({
                     'n_spheres': level,
---
+++ scripts/visualize.py
@@ -0,0 +1,48 @@
+from json import load as jsload
+
+from fire import Fire
+from trimesh.creation import icosphere
+from trimesh.exchange.load import load_mesh
+from trimesh.transformations import translation_matrix
+from trimesh.viewer import SceneViewer
+
+from common import *
+
+
+def main(mesh: str | None = None, spheres: str | None = None, level: int = 1):
+    scene = Scene()
+    if mesh:
+        mesh_filepath = Path(mesh)
+        if not mesh_filepath.exists:
+            raise RuntimeError(f"Path {mesh} does not exist!")
+
+        loaded_mesh = as_mesh(load_mesh(mesh_filepath,
+                                        process=False))  # type: ignore
+
+        scene.add_geometry(loaded_mesh)
+
+    if spheres:
+        sphere_filepath = Path(spheres)
+        if not sphere_filepath.exists:
+            raise RuntimeError(f"Path {spheres} does not exist!")
+
+        with open(sphere_filepath, 'r') as json_file:
+            data = jsload(json_file, cls=SphereDecoder)
+
+        if level > len(data):
+            raise RuntimeError(
+                f"Level {level} greater than available ({len(data)})!")
+        sphere_data = data[level]['spheres']
+
+        for sphere in sphere_data:
+            sphere_mesh = icosphere(radius=sphere.radius)
+            sphere_mesh.visual.face_colors = [200, 200, 250, 100]
+            scene.add_geometry(sphere_mesh,
+                               transform=translation_matrix(sphere.origin))
+
+    if mesh or spheres:
+        SceneViewer(scene)
+
+
+if __name__ == "__main__":
+    Fire(main)
